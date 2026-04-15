1699f638262a5dca8374a15d002afdc48b7fc8b8
Thinh <thinh0704hcm@users.noreply.github.com>
2026-04-15 15:40:47 +0700

    minor: removed duplicate import

--- backend/src/main/java/io/zalord/user/repository/UserRepository.java
+++ backend/src/main/java/io/zalord/user/repository/UserRepository.java
@@ -6,8 +6,6 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import io.zalord.user.domain.entities.User;
-
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByPhoneNumber(String phoneNumber);
     Optional<User> findByEmail(String email);
